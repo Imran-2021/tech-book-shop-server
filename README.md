3816e3ad6f028acc9981e0574d9d4f3f4c7ce0b1
next-hero <mdimranulhaque.8dec20@gmail.com>
2021-04-21 10:13:53 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# simple-full-stack-web-practices--asin-10--tech-book-shop-server
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+const app = express()
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const port = process.env.PORT ||5000;
+app.use(cors());
+app.use(bodyParser.json())
+
+const uri = `mongodb+srv://${process.env.DV_User}:${process.env.DV_Password}@cluster0.hosss.mongodb.net/${process.env.DV_Name}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Web world!')
+})
+
+client.connect(err => {
+  const EventCollection = client.db("book-server").collection("server");
+  // perform actions on the collection object
+  console.log('database connnected successfully !!');
+});
+
+app.listen(port)
